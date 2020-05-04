ec416ff7b32fa6ba45e07c7b8c9d4ec0d012bba4
Dr. ERDI Gergo <gergo@erdi.hu>
2020-05-04 23:02:36 +0800

    Clashilator as a library

---
+++ src/Clash/Clashilator.hs
@@ -0,0 +1,126 @@
+{-# LANGUAGE RecordWildCards, OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE ApplicativeDo #-}
+module Clash.Clashilator (generateFiles) where
+
+import Clash.Driver.Types
+
+import Data.Maybe (fromMaybe)
+import Data.Char (isDigit)
+import Control.Monad (forM_)
+
+import Text.Regex.Applicative
+
+import System.FilePath
+import System.Directory
+
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
+import Development.Shake (writeFileChanged)
+
+import Text.Mustache
+import qualified Text.Mustache.Compile.TH as TH
+import Data.Aeson hiding (Options)
+import qualified Data.HashMap.Strict as H
+
+data Port = Port Text Int
+    deriving Show
+
+data FFIType
+    = FFIBit
+    | FFIU8
+    | FFIU16
+    | FFIU32
+    | FFIU64
+
+ffiType :: Int -> FFIType
+ffiType 1 = FFIBit
+ffiType n
+  | n <= 8 = FFIU8
+  | n <= 16 = FFIU16
+  | n <= 32 = FFIU32
+  | n <= 64 = FFIU64
+  | otherwise = error $ unwords ["ffiType:", show n]
+
+cType :: FFIType -> Text
+cType FFIBit = "bit"
+cType FFIU8 = "uint8_t"
+cType FFIU16 = "uint16_t"
+cType FFIU32 = "uint32_t"
+cType FFIU64 = "uint64_t"
+
+hsType :: FFIType -> Text
+hsType FFIBit = "Bit"
+hsType FFIU8 = "Word8"
+hsType FFIU16 = "Word16"
+hsType FFIU32 = "Word32"
+hsType FFIU64 = "Word64"
+
+cName :: Text -> Text
+cName = id
+
+hsName :: Text -> Text -> Text
+hsName tag s = tag <> s
+
+parsePort :: Text -> Text -> Port
+parsePort name ty = Port name $ fromMaybe (error err) $ match re (T.unpack ty)
+  where
+    err = unwords ["Invalid port type:", show ty]
+    re = pure 1 <|> (bitWidth <$> bus)
+    bus = sym '[' *> ((,) <$> num <* sym ':' <*> num) <* sym ']'
+    num = read <$> many (psym isDigit)
+
+    bitWidth (a, b)
+        | a < b = b - a + 1
+        | otherwise = a - b + 1
+
+removeClock :: [Port] -> (Maybe Text, [Port])
+removeClock (Port clk 1 : ps) = (Just clk, ps)
+removeClock ps = (Nothing, ps)
+
+portInfo :: Text -> Port -> Value
+portInfo tag (Port name width) = object
+    [ "cName" .= cName name
+    , "cType" .= cType ty
+    , "hsName" .= hsName tag name
+    , "hsType" .= hsType ty
+    ]
+  where
+    ty = ffiType width
+
+manifestInfo :: FilePath -> FilePath -> Manifest -> Value
+manifestInfo inputDir outputDir Manifest{..} = object
+    [ "inPorts"      .= (markEnds $ map (portInfo "i") ins)
+    , "outPorts"     .= (markEnds $ map (portInfo "o") outs)
+    , "clock"        .= fmap (\clock -> object ["cName" .= cName clock]) clock
+    , "outDir"       .= TL.pack outputDir
+    , "srcs"         .= [ object ["verilogPath" .= TL.pack (inputDir </> T.unpack component <.> "v")]
+                        | component <- componentNames
+                        ]
+    ]
+  where
+    (clock, ins) = removeClock $ zipWith parsePort portInNames portInTypes
+    outs = zipWith parsePort portOutNames portOutTypes
+
+markEnds :: [Value] -> [Value]
+markEnds [] = []
+markEnds (v:vs) = markStart v : vs
+  where
+    markStart (Object o) = Object $ o <> H.fromList [ "first" .= True ]
+
+templates =
+    [ ("csrc/Interface.h", $(TH.compileMustacheFile "template/Interface.h.mustache"))
+    , ("csrc/Impl.cpp", $(TH.compileMustacheFile "template/Impl.cpp.mustache"))
+    , ("csrc/Impl.h", $(TH.compileMustacheFile "template/Impl.h.mustache"))
+    , ("csrc/Makefile",  $(TH.compileMustacheFile "template/Makefile.mustache"))
+    , ("Setup.hs",  $(TH.compileMustacheFile "template/Setup.hs.mustache"))
+    , ("src/VerilatorFFI.hsc", $(TH.compileMustacheFile "template/VerilatorFFI.hsc.mustache"))
+    , ("package.yaml",  $(TH.compileMustacheFile "template/package.yaml.mustache"))
+    ]
+
+generateFiles :: FilePath -> FilePath -> Manifest -> IO ()
+generateFiles inputDir outputDir manifest = do
+    let vals = manifestInfo inputDir outputDir manifest
+    forM_ templates $ \(fname, template) -> do
+        writeFileChanged (outputDir </> fname) $ TL.unpack $ renderMustache template vals
